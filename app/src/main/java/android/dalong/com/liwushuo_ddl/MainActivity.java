bad3ae66cbae9937670f699fb413b30535d90272
ddl758217 <dengmenglong@126.com>
2016-06-27 19:29:08 +0800

    这是我的礼物说项目

---
+++ app/src/main/java/android/dalong/com/liwushuo_ddl/MainActivity.java
@@ -0,0 +1,17 @@
+package android.dalong.com.liwushuo_ddl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
